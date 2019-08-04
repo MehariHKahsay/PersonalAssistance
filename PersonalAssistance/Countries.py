d5a7fe3662144079e7a31367d058b99daade2686
Mehari Kahsay <mehari.kahsay@mnsu.edu>
2019-08-03 19:33:55 -0500

    User Queries method created

--- PersonalAssistance/Countries.py
+++ PersonalAssistance/Countries.py
@@ -1,9 +1,9 @@
-class Country:
+class Countries:
     """description of class"""
-    def __init__(self, name, location, capital, currency, population, area):
+    def __init__(self, name, location, capital_city, currency, population, area):
         self.name = name
         self.location = location
-        self.capital_city = capital
+        self.capital_city = capital_city
         self.currency = currency
         self.population = population
         self.area = area       
--- PersonalAssistance/Database.py
+++ PersonalAssistance/Database.py
@@ -1,82 +1,82 @@
 import sqlite3
-from Country import Country
+from Countries import Countries
 
 conn = sqlite3.connect(':memory:')
-#conn = sqlite3.connect('Country.db')
+#conn = sqlite3.connect('Countries.db')
 c = conn.cursor()
-c.execute("""CREATE TABLE country(
-name text,
-location text,
-capital_city text,
-currency text,
-population integer,
-area integer)""")
+c.execute("""CREATE TABLE countries(
+            name text,
+            location text,
+            capital_city text,
+            currency text,
+            population integer,
+            area integer)""")
  
-canada = Country('Canada','North America','Ottawa', 'Canadian dollar', 32268240, 9970610)
-eritrea = Country('Eritrea','Africa','Asmara', 'Nakfa', 4401357, 117600)
-iraq = Country('Iraq','Asia','Baghdad', 'Iraqi dinar', 28807190, 438317)
-jordan = Country('Jordan','Asia','Amman', 'Jordanian dinar', 5702776, 89342)
-cuba = Country('Cuba','North America','Havana', 'Cuban peso', 11269400, 110861)
-sudan = Country('Sudan','Africa','Khartoum', 'Sudanese dinar', 36232950, 2505813)
-peru = Country('Peru','South America','Lima', 'Peruvian nuevo sol', 27968240, 1285216)
+canada = Countries('Canada','North America','Ottawa', 'Canadian dollar', 32268240, 9970610)
+eritrea = Countries('Eritrea','Africa','Asmara', 'Nakfa', 4401357, 117600)
+iraq = Countries('Iraq','Asia','Baghdad', 'Iraqi dinar', 28807190, 438317)
+jordan = Countries('Jordan','Asia','Amman', 'Jordanian dinar', 5702776, 89342)
+cuba = Countries('Cuba','North America','Havana', 'Cuban peso', 11269400, 110861)
+sudan = Countries('Sudan','Africa','Khartoum', 'Sudanese dinar', 36232950, 2505813)
+peru = Countries('Peru','South America','Lima', 'Peruvian nuevo sol', 27968240, 1285216)
 
-c.execute("INSERT INTO country values(?,?,?,?,?,?)", (canada.name, canada.location, canada.capital_city, canada.currency, canada.population, canada.area))
+c.execute("INSERT INTO countries values(?,?,?,?,?,?)", (canada.name, canada.location, canada.capital_city, canada.currency, canada.population, canada.area))
 conn.commit()
-c.execute("INSERT INTO country values(?,?,?,?,?,?)", (eritrea.name, eritrea.location, eritrea.capital_city, eritrea.currency, eritrea.population, eritrea.area))
+c.execute("INSERT INTO countries values(?,?,?,?,?,?)", (eritrea.name, eritrea.location, eritrea.capital_city, eritrea.currency, eritrea.population, eritrea.area))
 conn.commit()
-c.execute("INSERT INTO country values(?,?,?,?,?,?)", (iraq.name, iraq.location, iraq.capital_city, iraq.currency, iraq.population, iraq.area))
+c.execute("INSERT INTO countries values(?,?,?,?,?,?)", (iraq.name, iraq.location, iraq.capital_city, iraq.currency, iraq.population, iraq.area))
 conn.commit()
-c.execute("INSERT INTO country values(?,?,?,?,?,?)", (jordan.name, jordan.location, jordan.capital_city, jordan.currency, jordan.population, jordan.area))
+c.execute("INSERT INTO countries values(?,?,?,?,?,?)", (jordan.name, jordan.location, jordan.capital_city, jordan.currency, jordan.population, jordan.area))
 conn.commit()
-c.execute("INSERT INTO country values(?,?,?,?,?,?)", (cuba.name, cuba.location, cuba.capital_city, cuba.currency, cuba.population, cuba.area))
+c.execute("INSERT INTO countries values(?,?,?,?,?,?)", (cuba.name, cuba.location, cuba.capital_city, cuba.currency, cuba.population, cuba.area))
 conn.commit()
-c.execute("INSERT INTO country values(?,?,?,?,?,?)", (sudan.name, sudan.location, sudan.capital_city, sudan.currency, sudan.population, sudan.area))
+c.execute("INSERT INTO countries values(?,?,?,?,?,?)", (sudan.name, sudan.location, sudan.capital_city, sudan.currency, sudan.population, sudan.area))
 conn.commit()
-c.execute("INSERT INTO country values(?,?,?,?,?,?)", (peru.name, peru.location, peru.capital_city, peru.currency, peru.population, peru.area))
+c.execute("INSERT INTO countries values(?,?,?,?,?,?)", (peru.name, peru.location, peru.capital_city, peru.currency, peru.population, peru.area))
 
 def countries():
     #return list of countries
     with conn:
-        c.execute("SELECT name FROM country")
+        c.execute("SELECT name FROM countries")
         result = c.fetchall()
         print(result)
 def locations():
     #return list of continents
         with conn:
-            c.execute("SELECT location FROM country")
+            c.execute("SELECT location FROM countries")
             result = c.fetchall()
             print(result)
 
 def capitalCities():
     #return list of capital cities
         with conn:
-            c.execute("SELECT capital FROM country")
+            c.execute("SELECT capital FROM countries")
             result = c.fetchall()
             print(result)
 
 def currencies():
     #return list of currencies
         with conn:
-            c.execute("SELECT currency FROM country")
+            c.execute("SELECT currency FROM countries")
             result = c.fetchall()
             print(result)
 
 def population():
     #returns population
         with conn:
-            c.execute("SELECT population FROM country")
+            c.execute("SELECT population FROM countries")
             result = c.fetchall()
             print(result)
 
 def area():
 #returns population
     with conn:
-        c.execute("SELECT area FROM country")
+        c.execute("SELECT area FROM countries")
         result = c.fetchall()
         print(result)
 
 def displayQueries():
-    user = input("Pleae type NAME, LOCATION, CURRENCY, CAPITAL CITY, POPULATION, or AREA: ")
+    user = input("Pleae type NAME, LOCATION, CURRENCY, CAPITAL_CITY, POPULATION, or AREA: ")
     if user.upper() == "NAME":
         countries()
     elif user.upper() == "LOCATION":
@@ -93,16 +93,70 @@ def displayQueries():
         print("Wrong input...")
         displayQueries()
 
+def nameOfcountries(countries):
+        with conn:
+            c.execute("SELECT name FROM countries WHERE upper(name) =:name",{'name':countries})
+            result = c.fetchall()
+            print(result)
+def locationOfcountries(countries):
+        with conn:
+            c.execute("SELECT location FROM countries WHERE upper(name) =:name",{'name':countries})
+            result = c.fetchall()
+            print(result)
+
+def currencyOfcountries(countries):
+        with conn:
+            c.execute("SELECT currency FROM countries WHERE upper(name) =:name",{'name':countries})
+            result = c.fetchall()
+            print(result)
+
+
+def capitalCityOfcountries(countries):
+        with conn:
+            c.execute("SELECT capital_city FROM countries WHERE upper(name) =:name",{'name':countries})
+            result = c.fetchall()
+            print(result)            
+
+def populationOfcountries(countries):
+        with conn:
+            c.execute("SELECT population FROM countries WHERE upper(name) =:name",{'name':countries})
+            result = c.fetchall()
+            print(result)
+
+def areaOfcountries(countries):
+        with conn:
+            c.execute("SELECT area FROM countries WHERE upper(name) =:name",{'name':countries})
+            result = c.fetchall()
+            print(result)
+
 def userQueries():
-    userinput = str(input("Type field of a Contry: Example \"Name of Canada\""))
-    splited = userinput.split(" ")
-    field = splited.index(0)
-    country = splited.index(2)
+        userinput = str(input("Type field of a Contry: Example \"Name of Canada\":"))
+        splited = userinput.split(" ")
+        print(splited)
+        field = splited[0]
+        uppercae = field.upper()
+        print(uppercae)
+        country = splited[2]
+        countriesname = country.upper()
 
-
-
+        if field.upper() == "NAME":
+            nameOfcountries(countriesname)
+        elif field.upper() == "LOCATION":
+            locationOfcountries(countriesname)
+        elif field.upper() == "CURRENCY":
+            currencyOfcountries(countriesname)
+        elif field.upper() == "CAPITAL_CITY":
+            capitalCityOfcountries(countriesname)
+        elif field.upper() == "POPULATION":
+            populationOfcountries(countriesname)
+        elif field.upper() == "AREA":
+            areaOfcountries(countriesname)
+        else:
+            print("Wrong input...")
+            userQueries() 
+ 
+userQueries()
 #displayQueries()
-
 conn.commit()
 conn.close()
 
